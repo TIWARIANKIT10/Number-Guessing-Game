98b02c4136da728fd0abd2fe41db145afb6663b4
TIWARIANKIT10 <00tiwariankit00@gmail.com>
2025-03-06 09:09:37 +0545

    java script code added and some adjustment in html also

---
+++ index.js
@@ -0,0 +1,37 @@
+ 
+ let randomnumber = Math.floor((Math.random()*10)+1);
+
+let attemptNumber= 0;
+function check(){
+ let input= document.getElementById('inputNumber');
+ let attempttag = document.getElementById('attempt');
+ let inputvalue = input.value;
+ attemptNumber++;
+ attempttag.textContent = "Attempt"+attemptNumber;
+ attempttag.style.color = "tomato"
+
+ if(inputvalue==randomnumber){
+    attempttag.textContent ="you have guessed right in attempt "+ attemptNumber;
+    attempttag.style.color = "green";
+ }
+
+else if (inputvalue>10){
+    attempttag.textContent = "you have entered to high number ";
+    attempttag.style.color="red";
+
+ }
+ else if (inputvalue<1){
+    attempttag.textContent="you have enterd to low number"
+    attempttag.style.color="red";
+ }
+
+}
+
+function reset(){
+    attemptNumber =0;
+ randomnumber = Math.floor((Math.random()*10)+1);
+ let attempttag = document.getElementById('attempt').textContent="";
+
+}
+
+
